87c9de2752a6ec7b41eaa182456e279737f92540
Mitchell Herrijgers <mitchellherrijgers@gmail.com>
2016-06-21 21:06:50 +0200

    Added screep-profiler library

--- src/main.js
+++ src/main.js
@@ -1,17 +1,22 @@
 // Load extensions
 require('./extension.creep');
+var profiler = require('./lib.profiler');
 
 import CreepManager from './manager.creep';
 import MemoryManager from './manager.memory';
 import TowerManager from './manager.tower';
 
+profiler.enable();
 module.exports.loop = function () {
-    PathFinder.use(true);
-    let managers = [new MemoryManager(), new CreepManager(), new TowerManager()];
+    profiler.wrap(function() {
+        PathFinder.use(true);
+        let managers = [new MemoryManager(), new CreepManager(), new TowerManager()];
 
-    _.each(managers, function(manager) {
-        if(manager.shouldExecute()) {
-            manager.manage();
-        }
+        _.each(managers, function(manager) {
+            if(manager.shouldExecute()) {
+                manager.manage();
+            }
+        });
     });
+
 };
